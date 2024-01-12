457ede2fe6180700adf454e509ec6577f7f15034
Matt Day <mattday@matts-mac-mini.lan>
2024-01-11 19:13:02 -0500

    added navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav>
+        <a>Home</a>
+        <a>Indian Springs Katahdins</a>
+        <a>Contact</a>
+    </nav>
+</template>
+
+<style scoped>
+    nav{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        height: 10vh;
+        background: black;
+        color: white;
+    }
+</style>
